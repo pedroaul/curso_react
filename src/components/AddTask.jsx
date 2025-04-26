5f4ea687e3a61c044f2a5b7b6e70b50d48e45fe6
pedroaul <red6190br@gmail.com>
2025-04-26 14:20:57 -0300

    v_final

--- src/components/AddTask.jsx
+++ src/components/AddTask.jsx
@@ -1,15 +1,16 @@
 import { useState } from "react";
+import Input from "./Input";
 
 function AddTask({onAddTaskSubmit}) {
     const [title, setTitle] = useState("");
     const [description, setDescription] = useState("");
     return (
     <div className="space-y-4 p-6 bg-slate-200 rounded-md shadow flex flex-col">
-        <input type="text" placeholder="Digite o título da tarefa" className="bg-white border-slate-300 outline-slate-400 px-4 py-2 rounded-md" 
+        <Input type="text" placeholder="Digite o título da tarefa"
         value={title}
         onChange={(event) => setTitle(event.target.value)}
         />
-        <input type="text" placeholder="Digite a descrição da tarefa" className="bg-white border-slate-300  outline-slate-400 px-4 py-2 rounded-md" 
+        <Input type="text" placeholder="Digite a descrição da tarefa"
         value={description}
         onChange={(event) => setDescription(event.target.value)}
         />
---
+++ src/components/Input.jsx
@@ -0,0 +1,13 @@
+function Input(props) {
+    return (
+        <input 
+            type={props.type}
+            placeholder={props.placeholder}
+            className="border bg-white border-slate-300 outline-slate-400 px-4 py-2 rounded-md"
+            value={props.value}
+            onChange={props.onChange}
+        />
+    );
+}
+
+export default Input;
